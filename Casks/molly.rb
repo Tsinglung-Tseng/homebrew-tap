e6df124ccea0a50370e536e80e87e3bd35b93b72
Tsinglung Tseng <tsinglung.tseng@gmail.com>
2026-03-30 20:52:07 +0800

    chore: update molly sha256 for v1.0.0

--- Casks/molly.rb
+++ Casks/molly.rb
@@ -1,6 +1,6 @@
 cask "molly" do
   version "1.0.0"
-  sha256 "ca2c39183118e5f5cbcf89606082f209fde6f695e0e5175c20be2cbbe75f7e5b"
+  sha256 "accd635335040a2e6a41cb45b337ab20e748834af86424ed377e529a1c3c57ec"
 
   url "https://github.com/Tsinglung-Tseng/molly/releases/download/v#{version}/Molly-#{version}.zip"
   name "Molly"
